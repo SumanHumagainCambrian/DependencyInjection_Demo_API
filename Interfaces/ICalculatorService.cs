a141968c57c016044f0d9e200f0c49d3c124db62
Suman Humagain <returnzerosh@gmail.com>
2024-10-03 14:51:28 -0400

    Added Calculator service interface

---
+++ Interfaces/ICalculatorService.cs
@@ -0,0 +1,10 @@
+﻿namespace DependencyInjection_Demo_API.Interfaces
+{
+    public interface ICalculatorService
+    {
+        decimal Add(decimal num1, decimal num2);
+        decimal Subtract(decimal num1, decimal num2);
+        decimal Multiply(decimal num1, decimal num2);
+        decimal Divide(decimal num1, decimal num2);
+    }
+}
